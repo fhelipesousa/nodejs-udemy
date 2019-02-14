0d8653ed6bbfd18e054e040591758dafb11e83e9
Felipe Sousa <fhelipesousa@gmail.com>
2019-02-14 09:03:20 -0300

    node-js com body-parser

---
+++ app/routes/admin.js
@@ -0,0 +1,18 @@
+module.exports = function(application){
+	application.get('/formulario_inclusao_noticia', function(req, res){
+		res.render("admin/form_add_noticia");
+	});
+
+	
+	application.post('/noticias/salvar', function(req, res){
+		var noticia = req.body;
+
+		var connection = application.config.dbConnection();
+        var noticiaModel = application.app.models.noticiasModel;
+
+        noticiaModel.salvarNoticia(noticia, connection, function(error, result){			
+			res.redirect('/noticias');
+		});		
+
+	});
+}
